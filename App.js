d0d412d3461e27ed4efb3d1b03c62f42649cfe86
Tomáš Klíma <tomas.klim@gmail.com>
2018-11-25 10:55:04 +0100

    add app skeleton

---
+++ App.js
@@ -0,0 +1,16 @@
+// @flow
+import React from 'react'
+import { StyleSheet, Text, SafeAreaView } from 'react-native'
+import Expo from 'expo'
+
+// containers
+
+export default class App extends React.PureComponent<null> {
+  render() {
+    return (
+      <SafeAreaView>
+        <Text>aa</Text>
+      </SafeAreaView>
+    )
+  }
+}
