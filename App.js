d304fde487cb1740ac6d1458ed138b1aace08b22
Tomáš Klíma <tomas.klim@gmail.com>
2018-12-22 11:19:55 +0100

    Persist store

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 // @flow
-import 'rxjs'
 
 // react
 import React from 'react'
@@ -10,22 +9,36 @@ import { createStore, applyMiddleware } from 'redux'
 import { createLogger } from 'redux-logger'
 import reduxThunk from 'redux-thunk'
 import reduxPromise from 'redux-promise'
+import { persistStore, persistReducer } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
+import storage from 'redux-persist/lib/storage'
+
 import rootReducer from './src/state/reducers'
 
 // components
 import Navigator from './src/containers/navigators/Navigator'
 
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 const logger = createLogger({ collapsed: true })
 const store = createStore(
-  rootReducer,
+  persistedReducer,
   applyMiddleware(reduxThunk, reduxPromise, logger),
 )
+const persistor = persistStore(store)
 
 export default class App extends React.PureComponent<null> {
   render() {
     return (
       <Provider store={store}>
-        <Navigator />
+        <PersistGate loading={null} persistor={persistor}>
+          <Navigator />
+        </PersistGate>
       </Provider>
     )
   }
